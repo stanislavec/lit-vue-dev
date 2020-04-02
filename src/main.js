ac32885d967abd971e4b1fbea817f436a968317c
Stanislav Kryzhanovsky <stanislavkryzhanovsky@gmail.com>
2020-04-02 20:07:42 +0300

    install vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: HelloWorld },
+  { path: '/test', component: Test }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes // сокращённая запись для `routes: routes`
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
