23fc1e9700a60870a9454e37c4353341e0771544
Stanislav Kryzhanovsky <stanislavkryzhanovsky@gmail.com>
2020-04-07 12:52:26 +0300

    component life-cycle

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,43 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+	<div id="app">
+		<div class="link-wrapper">
+			<router-link to="/">Home</router-link>
+			<router-link to="/test">Test Component</router-link>
+		</div>
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+	name: 'App',
+};
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+.link-wrapper {
+	width: 100%;
+	background-color: #2c3e50;
+	padding: 10px 20px;
+}
+
+a {
+	color: white;
+	text-decoration: unset;
+	padding: 5px 10px;
+}
+
+a:hover {
+	color: #2c3e50;
+	background-color: white;
 }
 </style>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,9 +1,50 @@
 <template>
-	<div>Test</div>
+	<div>
+		<h1>{{ text }}</h1>
+		<div>Test clicked {{ click }}</div>
+		<button @click="clickCount()">Посчитать клики</button>
+		<br />
+		<router-link to="/">Go home</router-link>
+	</div>
 </template>
 
 <script>
 export default {
-	name: 'Test'
-}
+	name: 'Test',
+	data() {
+		return {
+			text: 'Main Title',
+			click: 0,
+		};
+	},
+	methods: {
+		clickCount() {
+			return (this.click += 1);
+		},
+	},
+	beforeCreate() {
+		console.log('beforeCreate', 1);
+	},
+	created() {
+		console.log('created', 2);
+	},
+	beforeMount() {
+		console.log('beforeMount', 3);
+	},
+	mounted() {
+		console.log('mounted', 4);
+	},
+	beforeUpdate() {
+		console.log('beforeUpdate', 5);
+	},
+	updated() {
+		console.log('updated', 6);
+	},
+	beforeDestroy() {
+		console.log('beforeDestroy', 7);
+	},
+	destroyed() {
+		console.log('destroyed', 8);
+	},
+};
 </script>
